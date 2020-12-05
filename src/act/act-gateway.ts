d72d875b06601b8f24aefbb1201e714b02068b48
Fredrik Pettersson <fredrik.pettersson@lotto24.de>
2020-12-05 09:22:11 +0000

    Add middy middlewares

--- src/act/act-gateway.ts
+++ src/act/act-gateway.ts
@@ -1,27 +1,17 @@
-import { APIGatewayEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { middify } from 'src/shared/api/third-party.middleware';
 import { createResponse } from '../shared/api/response.factory';
 import { ActController } from './api/act.controller';
 import { ActDocumentMapper } from './data-access/act-document.mapper';
 import { ActRepository } from './data-access/act.repository';
-import { Act } from './model/act';
 
 const controller = new ActController(new ActRepository(new ActDocumentMapper()));
 
-export async function createAct(event: APIGatewayEvent): Promise<APIGatewayProxyResult> {
-  try {
-    const act: Act = JSON.parse(event.body);
-    await controller.create(act);
-    return createResponse(200);
-  } catch (error) {
-    return createResponse(500, { message: error?.message });
-  }
-}
+export const createAct = middify(async (event) => {
+  await controller.create(event.body);
+  return createResponse(201);
+});
 
-export async function listActs(): Promise<APIGatewayProxyResult> {
-  try {
-    const result = await controller.list();
-    return createResponse(200, result);
-  } catch (error) {
-    return createResponse(500, { message: error?.message });
-  }
-}
+export const listActs = middify(async () => {
+  const result = await controller.list();
+  return createResponse(200, result);
+});
---
+++ src/shared/api/third-party.middleware.ts
@@ -0,0 +1,12 @@
+import middy from '@middy/core';
+import httpErrorHandler from '@middy/http-error-handler';
+import httpEventNormalizer from '@middy/http-event-normalizer';
+import httpHeaderNormalizer from '@middy/http-header-normalizer';
+import jsonBodyParser from '@middy/http-json-body-parser';
+
+export const middify = (handler) =>
+  middy(handler)
+    .use(jsonBodyParser())
+    .use(httpHeaderNormalizer())
+    .use(httpEventNormalizer())
+    .use(httpErrorHandler());
